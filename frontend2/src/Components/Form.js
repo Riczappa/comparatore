7a3626a1ff3eafc476c121e56c765b5fa820a3be
Riczappa <126017896+Riczappa@users.noreply.github.com>
2023-11-12 21:47:52 -0800

    ss

--- frontend2/src/Components/Form.js
+++ frontend2/src/Components/Form.js
@@ -16,7 +16,7 @@ function FormStep1({ onNext, formData, updateFormData }) {
             onChange={(e) => updateFormData('capital', e.target.value)} 
             placeholder="Inserisci l'importo" 
           />
-          <button className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700">Avanti</button>
+          <button className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700"  onClick={onNext}>Avanti</button>
         </div>
       </div>
     );
