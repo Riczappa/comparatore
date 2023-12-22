0b72e8c4f988d57ddb52cd606a089fdb84b7e344
Riczappa <126017896+Riczappa@users.noreply.github.com>
2023-12-22 16:41:32 +0100

    ss

--- frontend2/src/Components/Form.js
+++ frontend2/src/Components/Form.js
@@ -9,20 +9,21 @@ function FormStep1({ onNext, formData, updateFormData, handleSuggestionClick }) 
     <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex flex-col items-center"  style={{ height: "220px" }}>
       <div className="w-full text-center">
         <h2 className="text-lg md:text-xl font-medium text-black">Quanto vuoi investire?</h2>
-        <input
-          type="number"
-          className="mt-1 md:mt-2 p-2 border rounded-lg w-full"
-          value={formData.capital}
-          onChange={(e) => updateFormData('capital', e.target.value)}
-          placeholder="Inserisci l'importo"
-        />
+       
         <div className="flex space-x-2 mt-2 justify-center">
           {/* Esempi di suggerimenti */}
-          <button className="px-3 py-1 border rounded text-sm hover:bg-green-500 hover:text-white" onClick={() => handleSuggestionClick('capital', 5000)}>€5.000</button>
-          <button className="px-3 py-1 border rounded text-sm hover:bg-green-500 hover:text-white" onClick={() => handleSuggestionClick('capital', 10000)}>€10.000</button>
-          <button className="px-3 py-1 border rounded text-sm hover:bg-green-500 hover:text-white" onClick={() => handleSuggestionClick('capital', 20000)}>€20.000</button>
+          <button className="suggestion-button" onClick={() => handleSuggestionClick('capital', 5000)}>€5.000</button>
+          <button className="suggestion-button" onClick={() => handleSuggestionClick('capital', 10000)}>€10.000</button>
+          <button className="suggestion-button" onClick={() => handleSuggestionClick('capital', 20000)}>€20.000</button>
         </div>
-        <button className="mt-4 px-4 py-1 bg-green-400 text-white rounded hover:bg-green-600" onClick={onNext}>Avanti</button>
+        <input
+          type="number"
+          className="mt-2 p-2 border rounded-lg w-full"
+          value={formData.capital}
+          onChange={(e) => updateFormData('capital', e.target.value)}
+          placeholder="Oppure selezionalo manualmente"
+        />
+        <button className=" next-button mt-4" onClick={onNext}>Avanti</button>
       </div>
     </div>);
 }
@@ -34,24 +35,26 @@ function FormStep2({ onBack, onNext, formData, updateFormData, handleSuggestionC
   return (
     <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex flex-col items-center"  style={{ height: "220px" }}>
       <h2 className="text-lg md:text-xl font-medium text-black text-center">Per quanti anni vuoi investire?</h2>
+     
+      <div className="flex space-x-2 mt-2">
+        {/* Esempi di suggerimenti */}
+        <button className="suggestion-button" onClick={() => handleSuggestionClick('years', 2)}>2 yrs</button>
+        <button className="suggestion-button" onClick={() => handleSuggestionClick('years', 5)}>5 yrs</button>
+        <button className="suggestion-button" onClick={() => handleSuggestionClick('years', 10)}>10 yrs</button>
+       
+      </div>
       <input
         type="number"
-        className="mt-1 md:mt-2 p-2 border rounded-lg w-full"
+        className="mt-1 md:mt-2 p-2 border rounded-lg w-64"
         min="1"
         max="30"
         value={formData.years}
         onChange={(e) => updateFormData('years', e.target.value)}
-        placeholder="Inserisci il numero di anni"
+        placeholder="Oppure Inserisci manualmente"
       />
-      <div className="flex space-x-2 mt-2">
-        {/* Esempi di suggerimenti */}
-        <button className="px-3 py-1 border rounded text-sm hover:bg-green-500 hover:text-white" onClick={() => handleSuggestionClick('years', 2)}>2 yrs</button>
-        <button className="px-3 py-1 border rounded text-sm hover:bg-green-500 hover:text-white" onClick={() => handleSuggestionClick('years', 5)}>5 yrs</button>
-        <button className="px-3 py-1 border rounded text-sm hover:bg-green-500 hover:text-white" onClick={() => handleSuggestionClick('years', 10)}>10 yrs</button>
-      </div>
       <div className="flex space-x-4 mt-4">
-        <button className="px-4 py-1 bg-gray-300 text-white rounded hover:bg-gray-500" onClick={onBack}>Indietro</button>
-        <button className="px-4 py-1 bg-green-400 text-white rounded hover:bg-green-600" onClick={onNext}>Avanti</button>
+        <button className="next-button" onClick={onBack}>Indietro</button>
+        <button className="back-button" onClick={onNext}>Avanti</button>
       </div>
     </div>
   );
@@ -67,14 +70,14 @@ function FormStep3({ onBack, onNext, formData, updateFormData, handleSuggestionC
         <div className='flex-grow text-center'>
           <h2 className="text-lg md:text-xl font-medium text-black">Capitale vincolato?</h2>
           <div className="inline-flex space-x-2 mt-2 justify-center">
-            <button className="px-3 py-1 border rounded text-sm hover:bg-green-500 hover:text-white" onClick={() => handleSuggestionClick('vincolato', "si")}>si</button>
-            <button className="px-3 py-1 border rounded text-sm hover:bg-green-500 hover:text-white" onClick={() => handleSuggestionClick('vincolato', "no")}>no</button>
-            <button className="px-3 py-1 border rounded text-sm hover:bg-green-500 hover:text-white" onClick={() => handleSuggestionClick('vincolato', "indif")}>indifferente</button>
+            <button className="suggestion-button" onClick={() => handleSuggestionClick('vincolato', "si")}>si</button>
+            <button className="suggestion-button" onClick={() => handleSuggestionClick('vincolato', "no")}>no</button>
+            <button className="suggestion-button" onClick={() => handleSuggestionClick('vincolato', "indif")}>indifferente</button>
           </div>
         </div>
         <div className="flex justify-center space-x-4 mt-4">
-          <button className="px-4 py-1 bg-gray-300 text-white rounded hover:bg-gray-500" onClick={onBack}>Indietro</button>
-          <button className="px-4 py-1 bg-green-400 text-white rounded hover:bg-green-600" onClick={onNext}>Avanti</button>
+          <button className="next-button" onClick={onBack}>Indietro</button>
+          <button className="back-button" onClick={onNext}>Avanti</button>
         </div>
       </div>
 
