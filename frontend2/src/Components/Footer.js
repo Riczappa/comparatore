f530cc9f1c12a2376d12b90d376b4c46482212ab
Riczappa <126017896+Riczappa@users.noreply.github.com>
2024-01-23 15:33:12 +0100

    nnn

--- frontend2/src/Components/Footer.js
+++ frontend2/src/Components/Footer.js
@@ -11,14 +11,18 @@ function Footer() {
         <div className="flex flex-col md:flex-row md:items-center md:space-x-4">
             <a href="/terms.pdf" target="_blank" className="text-gray-600 text-sm hover:underline mb-1 md:mb-0">Terms & Conditions</a>
             <a href="/Privacypolicy.pdf" target="_blank" className="text-gray-600 text-sm hover:underline mb-1 md:mb-0">Privacy Policy</a>
-            <a href="/contact" className="text-gray-600 text-sm hover:underline">Contact us</a>
+            <a href="mailto:comparatore@trepdigitalx.com?subject=Richiesta di Contatto&body=Ciao, vorrei saperne di più su..." className="text-gray-600 text-sm hover:underline">Contact us</a>
+
         </div>
            {/* Left Column for Logo and Site Name */}
+           <div className="flex flex-col">
            <div className="mt-4 md:mb-0 flex flex-row items-center">
             <img src={Logo} alt="Logo" className="h-8 pr-3" /> {/* Replace logoPath with your logo's path */}
             <p className="text-gray-700 text-sm">
                 &copy; {new Date().getFullYear()} Miglioricontideposito.it All rights reserved.
             </p>
+            </div>
+            <p>P. IVA 04717030169</p>
         </div>
     </div>
 </footer>
